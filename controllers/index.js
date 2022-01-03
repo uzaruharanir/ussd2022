b5d01f7106dec40c5c9500549e060d195f2fe086
Uzaruharanira Marc <33791244+uzarwanda@users.noreply.github.com>
2022-01-03 12:40:54 +0200

    choose

--- controllers/index.js
+++ controllers/index.js
@@ -133,8 +133,6 @@ export const main = async (req, res, error) => {
         break;
       } else {
         response = "CON Choose the district";
-         for (let i = 0; i < districts.length; i++) {
-          response = response + `\n${i + 1}. ${districts[i].district}`;
         break;
       }
     }
